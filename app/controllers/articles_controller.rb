ea2c8de784deb1a415617eea18b1d08f7e142177
Matt Rigdon <mrigdon@fingerprintplay.com>
2017-07-18 17:31:04 -0700

    added update articles update and deleted welcome stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,15 @@
 class ArticlesController < ApplicationController
   def update
+    Article.find(params[:id]).update! article_params
+
+    redirect_to featured_path
+  rescue
+    redirect_to featured_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(%i[headline author body])
   end
 end
