91e08717d2cf3b4fcd1e20002b2488e39ed03aad
Matthew Rigdon <rigdonmr@gmail.com>
2017-07-19 22:51:47 -0700

    added update_workout featured action

--- app/controllers/featured_controller.rb
+++ app/controllers/featured_controller.rb
@@ -2,10 +2,17 @@ class FeaturedController < ApplicationController
   def index
     @articles = Article.all
     @videos = Video.all
+    @workouts = Workout.all
   end
 
   def update_videos
     Video.update_featured! params[:featured].values
+
+    redirect_to featured_path
+  end
+
+  def update_workout
+    Workout.update_featured! [params[:workout]]
 
     redirect_to featured_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get '/featured', to: 'featured#index'
   put '/featured/update_videos', to: 'featured#update_videos'
+  put '/featured/update_workout', to: 'featured#update_workout'
 
   resources :articles, only: %i[update]
 
